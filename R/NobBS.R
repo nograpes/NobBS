40fa36b5f620346a976ea6417211c1b145d18944
aman <aman.verma.mtl@gmail.com>
2021-03-03 16:29:30 -0500

    Allow more than 10000 samples from posterior

--- R/NobBS.R
+++ R/NobBS.R
@@ -279,13 +279,13 @@ NobBS <- function(data, now, units, onset_date, report_date, moving_window=NULL,
 
   t <- now.T
 
-  parameter_extract <- matrix(NA, nrow=10000)
+  parameter_extract <- matrix(NA, nrow=specs$nSamp)
 
   if("lambda"%in%specs$param_names){
     parameter_extract <- cbind(parameter_extract,mymod.dat %>% dplyr::select(select_vars(names(mymod.dat),starts_with(paste("lambda[",t,",",sep="")))))
   }
   if("beta.logged"%in%specs$param_names){
-    betas.logged<- matrix(NA,nrow=10000,ncol=(max_D+1))
+    betas.logged<- matrix(NA,nrow=specs$nSamp,ncol=(max_D+1))
     dimnames(betas.logged) = list(NULL,c(paste("Beta",c(0:max_D))))
     for(d in 0:max_D){
       betas.logged[,(d+1)] <- (mymod.dat %>% dplyr::select(select_vars(names(mymod.dat),starts_with(paste("beta.logged[",(d+1),"]",sep="")))))[,1]
